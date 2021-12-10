055fb65131fd52548f3b3aff5309f14b3f9ea874
Sam Williams <blackeye793@gmail.com>
2021-12-10 22:39:00 +0000

    Added recipe steps to create recipe page

---
+++ js/createRecipe_RecipeSteps.js
@@ -0,0 +1,56 @@
+var openAddRecipeDiv = document.querySelectorAll("#OpenRecipeStepAddition");
+var inputAreaDiv = document.getElementById("inputRecipeStepDiv");
+
+var addStepButton = document.getElementById("AddStepButton");
+var cancelStepButton = document.getElementById("CancelStepButton");
+
+var stepInput = document.getElementById("stepInfo")
+
+var alertCallout = document.getElementById("StepAlertCallout");
+var calloutText = document.getElementById("StepCalloutText");
+
+var stepTemplate = document.getElementById("RecipeStepTemplate");
+
+var stepList = document.getElementById("stepList");
+
+openAddRecipeDiv.forEach(button => {
+    button.onclick = function() {
+        if (inputAreaDiv.style.display == "none") {
+            inputAreaDiv.style.display = "block";
+        }
+    }
+})
+
+cancelStepButton.onclick = function () {
+    inputAreaDiv.style.display = "none";
+
+    stepInput.value = null;
+}
+
+addStepButton.onclick = function() {
+    var stepInfo = stepInput.value;
+
+    var allowedToContinue = true;
+
+    if (stepInfo == "") {
+        calloutText.textContent = "You can't have a blank step!"
+    }
+
+    if (allowedToContinue) {
+        var stepUl = stepTemplate.cloneNode();
+
+        stepUl.textContent = `${stepInfo}`;
+
+        stepList.appendChild(stepUl);
+        stepUl.style.display = "block";
+
+        inputAreaDiv.style.display = "none";
+    }
+    else {
+        alertCallout.style.display = "block";
+
+        setTimeout(() => {
+            alertCallout.style.display = "none";
+        }, 5000);
+    }
+}
